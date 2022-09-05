dedc76d3b0cd0db7a94aebc913c714bf11699edc
Anaku Senko <Anaku.Senko@gmail.com>
2022-09-04 22:59:38 -0400

    updated to support text

--- src/main/java/io/github/lordanaku/mana_demo/hud/ManaHudElement.java
+++ src/main/java/io/github/lordanaku/mana_demo/hud/ManaHudElement.java
@@ -1,21 +1,27 @@
 package io.github.lordanaku.mana_demo.hud;
 
-import io.github.lordanaku.anakus_status_bars.api.hudelements.HudElements;
+import io.github.lordanaku.anakus_status_bars.api.hudelements.IHudElement;
 import io.github.lordanaku.anakus_status_bars.api.hudelements.RenderHudFunctions;
 import io.github.lordanaku.anakus_status_bars.screen.gui.config.Settings;
 import io.github.lordanaku.anakus_status_bars.utils.ASBModUtils;
 import io.github.lordanaku.anakus_status_bars.utils.TextureRecords;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
+import me.shedaniel.clothconfig2.gui.entries.ColorEntry;
+import me.shedaniel.clothconfig2.gui.entries.FloatListEntry;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
 
-public class ManaHudElement implements HudElements {
+import static io.github.lordanaku.mana_demo.hud.RegisterHudElement.MANA;
+
+public class ManaHudElement implements IHudElement {
     private boolean renderSide;
-    private int progress;
 
     @Override
     public void renderBar() {
-        getManaProgress();
         RenderHudFunctions.drawDefaultBar(getSide(), ASBModUtils.getPosYMod(getSide()), TextureRecords.DEFAULT_BAR);
-        RenderHudFunctions.drawProgressBar(getSide(), ASBModUtils.getPosYMod(getSide()), TextureRecords.PROGRESS_BAR, progress, Settings.colorSettings.get("Mana"), Settings.alphaSettings.get("Mana"));
+        RenderHudFunctions.drawProgressBar(getSide(), ASBModUtils.getPosYMod(getSide()), TextureRecords.PROGRESS_BAR, getManaProgress(), Settings.colorSettings.get(MANA.name()), Settings.alphaSettings.get(MANA.name()));
     }
 
     @Override
@@ -24,36 +30,88 @@ public class ManaHudElement implements HudElements {
     }
 
     @Override
+    public void renderText() {
+        RenderHudFunctions.drawText("Text", getSide(), shouldRenderIcon(), ASBModUtils.getPosYMod(getSide()), Settings.textColorSettings.get(MANA.name()), 81);
+    }
+
+    @Override
     public boolean getSide() {
         return renderSide;
     }
 
     @Override
-    public HudElements setSide(boolean side) {
+    public IHudElement setSide(boolean side) {
         this.renderSide = side;
         return this;
     }
 
     @Override
     public boolean shouldRender() {
-        return Settings.shouldRenderSettings.get("Mana");
+        return Settings.shouldRenderSettings.get(MANA.name());
     }
 
     @Override
     public boolean shouldRenderIcon() {
-        return shouldRender() && Settings.iconSettings.get("Mana");
+        return shouldRender() && Settings.iconSettings.get(MANA.name());
+    }
+
+    @Override
+    public boolean shouldRenderText() {
+        return shouldRender() && Settings.textSettings.get(MANA.name());
+    }
+
+    @Override
+    public void registerSettings(ConfigCategory mainCategory, ConfigCategory iconCategory, ConfigCategory textCategory, ConfigCategory colorCategory, ConfigCategory textColorSettings, ConfigCategory alphaCategory, ConfigEntryBuilder entryBuilder) {
+        BooleanListEntry toggleManaBar = entryBuilder.startBooleanToggle(Text.translatable("option.mana_demo.mana_bar"), Settings.shouldRenderSettings.get(MANA.name()))
+                .setDefaultValue(MANA.shouldRender())
+                .setSaveConsumer(newValue -> Settings.shouldRenderSettings.replace(MANA.name(), newValue))
+                .build();
+        mainCategory.addEntry(toggleManaBar);
+
+        BooleanListEntry toggleManaIcon = entryBuilder.startBooleanToggle(Text.translatable("option.mana_demo.mana_icon"), Settings.iconSettings.get(MANA.name()))
+                .setDefaultValue(MANA.shouldRenderIcon())
+                .setSaveConsumer(newValue -> Settings.iconSettings.replace(MANA.name(), newValue))
+                .build();
+        iconCategory.addEntry(toggleManaIcon);
+
+        BooleanListEntry toggleManaText = entryBuilder.startBooleanToggle(Text.translatable("option.mana_demo.mana_text"), Settings.textSettings.get(MANA.name()))
+                .setDefaultValue(MANA.shouldRenderText())
+                .setSaveConsumer(newValue -> Settings.textSettings.replace(MANA.name(), newValue))
+                .build();
+        textCategory.addEntry(toggleManaText);
+
+        ColorEntry manaColor = entryBuilder.startColorField(Text.translatable("option.mana_demo.mana_color"), Settings.colorSettings.get(MANA.name()))
+                .setDefaultValue(MANA.color())
+                .setSaveConsumer(newValue -> Settings.colorSettings.replace(MANA.name(), newValue))
+                .build();
+        colorCategory.addEntry(manaColor);
+
+        ColorEntry manaTextColor = entryBuilder.startColorField(Text.translatable("option.mana_demo.mana_text_color"), Settings.textColorSettings.get(MANA.name()))
+                .setDefaultValue(MANA.color())
+                .setSaveConsumer(newValue -> Settings.textColorSettings.replace(MANA.name(), newValue))
+                .build();
+        textColorSettings.addEntry(manaTextColor);
+
+        FloatListEntry manaAlpha = entryBuilder.startFloatField(Text.translatable("option.mana_demo.mana_alpha"), Settings.alphaSettings.get(MANA.name()))
+                .setDefaultValue(MANA.alpha())
+                .setMin(0.0f)
+                .setMax(1.0f)
+                .setTooltip(Text.translatable("option.anakus_status_bars.alpha_tooltip"))
+                .setSaveConsumer(newValue -> Settings.alphaSettings.replace(MANA.name(), MathHelper.clamp(newValue, 0, 1)))
+                .build();
+        alphaCategory.addEntry(manaAlpha);
     }
 
     @Override
     public String name() {
-        return "Mana";
+        return MANA.name();
     }
 
-    private void getManaProgress() {
-        float mana = 50;
-        float manaMax = 100;
+    private int getManaProgress() {
+        float mana = 10f;
+        float manaMax = 20f;
         float ratio = Math.min(1, Math.max(0, mana / manaMax));
         int maxProgress = 81;
-        progress = Math.min(maxProgress, MathHelper.ceil(ratio * maxProgress) + 2);
+        return Math.min(maxProgress, MathHelper.ceil(ratio * maxProgress) + 2);
     }
 }
--- src/main/java/io/github/lordanaku/mana_demo/hud/RegisterHudElement.java
+++ src/main/java/io/github/lordanaku/mana_demo/hud/RegisterHudElement.java
@@ -1,26 +1,28 @@
 package io.github.lordanaku.mana_demo.hud;
 
 import io.github.lordanaku.anakus_status_bars.api.ModRegisterFunctions;
+import io.github.lordanaku.anakus_status_bars.api.hudelements.HudElementType;
 import io.github.lordanaku.anakus_status_bars.api.hudelements.RenderHudFunctions;
-import io.github.lordanaku.anakus_status_bars.screen.gui.config.ConfigFileHandler;
 import io.github.lordanaku.anakus_status_bars.screen.gui.config.Settings;
 import io.github.lordanaku.anakus_status_bars.utils.TextureRecord;
 import net.minecraft.util.Identifier;
 
 public class RegisterHudElement {
 
+    public static final HudElementType MANA = new HudElementType("mana", true, true, true, false, 0x007FFF, 1);
     public static final Identifier MANA_HUD_ELEMENT = new Identifier("mana_demo", "textures/gui/mana_hud_element.png");
     public static final TextureRecord MANA_ICON = new TextureRecord(MANA_HUD_ELEMENT, 99, 0, 9, 9, 256, 256);
 
     /* * Register calls must be made before setupHudElements() to work at start * */
     /* * Register calls must be made after readFromConfig() if you wish to use Anaku's Status Bars Mod Menu instead of your own * */
     public static void registerHudElement() {
-        ConfigFileHandler.readFromConfig();
-        ModRegisterFunctions.registerShouldRenderModSettings("Mana", true);
-        ModRegisterFunctions.registerColorModSettings("Mana", 0x007FFF);
-        ModRegisterFunctions.registerAlphaModSettings("Mana", 1.0f);
-        ModRegisterFunctions.registerIconModSettings("Mana", true);
-        ModRegisterFunctions.registerHudElementSide("Mana", true);
+        ModRegisterFunctions.registerShouldRenderModSettings(MANA.name(), MANA.shouldRender());
+        ModRegisterFunctions.registerIconModSettings(MANA.name(), MANA.shouldRenderIcon());
+        ModRegisterFunctions.registerTextModSettings(MANA.name(), MANA.shouldRenderText());
+        ModRegisterFunctions.registerColorModSettings(MANA.name(), MANA.color());
+        ModRegisterFunctions.registerTextColorModSettings(MANA.name(), MANA.color());
+        ModRegisterFunctions.registerAlphaModSettings(MANA.name(), MANA.alpha());
+        ModRegisterFunctions.registerHudElementSide(MANA.name(), MANA.side());
         ModRegisterFunctions.registerModHudElements(new ManaHudElement());
         Settings.setupHudElements();
     }
